9e6e953c547da9546422c8ea1c03554cd3c85cd6
kotria <slipperythompson@gmail.com>
2017-03-22 11:00:21 -0400

    Initial PHP debug and interpreter test

---
+++ example.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Today is " .date('l') . ". ";
+?>
+
+
+Today is <?php echo date("l"); ?>. Here's the latest news.
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index Test</title>
+</head>
+<body>
+    <h1>
+        Testing testing
+    </h1>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kotria
+ * Date: 3/21/17
+ * Time: 8:59 PM
+ */
+echo "This is parsed by PHP";
+?>
